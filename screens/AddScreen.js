affb596d10a8eb32e80d72b102c5faf58e9cde0b
neel216 <neelr216@gmail.com>
2020-05-20 02:19:37 -0400

    delete unnecessary task bar

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -41,13 +41,6 @@ export default function AddScreen() {
         </View>
       </ScrollView>
 
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
     </View>
   );
 }
