3b1cbd243c46a1e7f95f3dc142f5d34392d6b096
neel216 <neelr216@gmail.com>
2020-05-21 03:55:14 -0400

    Added formatting for browse screen; variable number of listings on browse screen

--- screens/BrowseScreen.js
+++ screens/BrowseScreen.js
@@ -1,172 +1,134 @@
-import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-
-import { MonoText } from '../components/StyledText';
+import { StyleSheet, Text, View, SectionList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function BrowseScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+  let response2 = {'13453234': {category: 'Tutoring - Computer Science',
+                                title: 'Computer Science Tutor Needed',
+                                college: 'UNC-Chapel Hill',
+                                pay: '8/hr',
+                                description: 'Looking for a computer science tutor who can assist with lessons relating to COMP 116. Specifically looking for guidance with data structures and object oriented programming in Python.'
+                               },
+                   '13545687': {category: 'Tutoring - Computer Science',
+                                title: 'Computer Science Tutor Needed',
+                                college: 'UNC-Chapel Hill',
+                                pay: '8/hr',
+                                description: 'Looking for a computer science tutor who can assist with lessons relating to COMP 116. Specifically looking for guidance with data structures and object oriented programming in Python.'
+                               },
+                   '13587946': {category: 'Tutoring - Computer Science',
+                                title: 'Computer Science Tutor Needed',
+                                college: 'UNC-Chapel Hill',
+                                pay: '8/hr',
+                                description: 'Looking for a computer science tutor who can assist with lessons relating to COMP 116. Specifically looking for guidance with data structures and object oriented programming in Python.'
+                               }
+                  };
+  let response = {category: 'Tutoring - Computer Science',
+                  title: 'Computer Science Tutor Needed',
+                  college: 'UNC-Chapel Hill',
+                  pay: '8/hr',
+                  description: 'Looking for a computer science tutor who can assist with lessons relating to COMP 116. Specifically looking for guidance with data structures and object oriented programming in Python.'
+                };
+  let response3 = [{ title: 'listings', data: [response,
+                                               response,
+                                               response,
+                                              ]
+                    }];
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/BrowseScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-    </View>
-  );
-}
-
-BrowseScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
+  /*
+  for (const [id, data] of Object.entries(response2)) {
+    console.log(data);
   }
-}
+  TODO: ADD AXIOS REQUEST HERE AND CLEAN IT UP FOR REACT USING THIS ITERATOR
+  */
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
+  return (
+    <SectionList
+      sections={response3}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Listing data={item} /> }
+      renderSectionHeader={() => null}
+    />
   );
 }
+//<View style={{ flexDirection: 'row' }}>
+const Listing = ({ data }) => (
+  <RectButton style={styles.listing}>
+      <View>
+        <View style={styles.listingTextContainer}>
+          <Text style={styles.categoryText}>{data.category}</Text>
+          <Text style={styles.titleText}>{data.title}</Text>
+          <Text style={styles.collegeText}>at {data.college}</Text>
+          <Text style={styles.payText}>${data.pay}</Text>
+          <Text style={styles.descriptionText}>{data.description}</Text>
+          <View style={styles.footerTextContainer}>
+            <View style={{flex: 1}}>
+              <Text style={styles.footerTextLeft}>Verified Student <Text style={{ color: '#7AD492' }}>✓</Text></Text>
+            </View>
+            <View style={{flex: 1}}>
+              <Text style={styles.footerTextRight}>1 hour ago</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+  </RectButton>
+);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
+  listing: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
     paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: 'rgba(122, 187, 236, 0.69)',
+    borderRadius: 20,
+    marginTop: 15,
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  categoryText: {
+    fontSize: 15,
+    color: 'grey',
+    alignSelf: 'flex-start',
   },
+  titleText: {
+    fontSize: 20,
+    alignSelf: 'flex-start',
+    marginTop: 2,
+  },
+  collegeText: {
+    fontSize: 15,
+    color: 'grey',
+    alignSelf: 'flex-start',
+    marginTop: 2,
+  },
+  payText: {
+    fontSize: 15,
+    color: '#7AD492',
+    alignSelf: 'flex-start',
+    marginTop: 2,
+    marginBottom: 5,
+  },
+  descriptionText: {
+    fontSize: 15,
+    color: 'grey',
+    alignSelf: 'flex-start',
+    marginTop: 2,
+    lineHeight: 20
+  },
+  footerTextContainer: {
+    flexDirection: 'row',
+    paddingTop: 6,
+  },
+  footerTextLeft: {
+    fontSize: 13,
+    color: 'grey',
+    marginTop: 5,
+    marginBottom: -10
+  },
+  footerTextRight: {
+    fontSize: 13,
+    color: 'grey',
+    marginTop: 5,
+    textAlign: 'right',
+    marginBottom: -10
+  }
 });
